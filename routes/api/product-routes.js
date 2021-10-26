93d44ccefe56435570ea23d4b51d8973b6f96d59
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-26 18:54:58 -0500

    started coding product-routes.js GET ALL PRODUCTS

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll({
+      include: [{
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      },
+    ]
+    });
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
