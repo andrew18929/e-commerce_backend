89fd21a252dc16141864e4b6556791f2390d0916
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-26 10:18:36 -0500

    defined columns on Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { truncate } = require('../config/connection.js');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,7 +8,18 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    tag_name: {
+      type: DataTypes.STRING
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
