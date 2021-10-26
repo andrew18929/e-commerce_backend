e38d21ef831fea5e442129c19507c6a9e3430145
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-26 18:44:44 -0500

    coded category-routes.js. may come back and shorten code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,16 +41,55 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const catData = await Category.create({
+      catergory_name: req.body.catergory_name
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await Category.update({
+      catergory_name: req.body.catergory_name
+    },
+    {
+    where: { id: req.params.id }
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'No category associated with this id!' });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'No category associated with this id!' });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
